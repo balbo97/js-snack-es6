444e8c034e85c948dff5fd03bf4436b2cd707f13
balbo97 <balbosauro1@gmail.com>
2023-12-06 19:16:03 +0100

    SNACK-2 completed

--- js/script.js
+++ js/script.js
@@ -32,7 +32,6 @@ bikes.forEach((elem)=>{
     
     // utilizzo il destructuring per determinare la costante peso 
     let {peso} = elem;
-    console.log(peso)
 
     // pusho i pesi nell'array vuoto creato in precedenza 
     peso_bici.push(peso)
@@ -43,3 +42,60 @@ console.log(peso_bici)
 // utilizzo la funxione Math.min per determinare il più piccolo dei valori nell'array
 let smallest_number = Math.min(...peso_bici)
 console.log(smallest_number)
+
+
+// SNACK-2 ----------------------->>
+
+// CREO UNA FUNZIONE CHE MI GENERI NUMERI RANDOM 
+function create_number(){
+    return Math.floor(Math.random()*100)+1;
+}
+
+// CREO UN ARRAY DI SQUADRE DI CALCIO 
+const soccer_team = [
+    {
+        nome : 'Juventus',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : 'Roma',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : 'Milan',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : 'Inter',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome : 'Napoli',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+]
+
+// CREO UN NUOVO ARRAY VUOTO 
+let array =[];
+
+// FACCIO UN CICLO FOR EACH 
+soccer_team.forEach((elem)=>{
+
+    // genero ed inserisco il numero casuale negli objs
+    elem.falliSubiti = create_number();
+    elem.puntiFatti = create_number();
+
+    // utilizzo il destructuring per determinare le variabili nome e falli subiti
+    let {nome, falliSubiti} = elem;
+
+    // pusho nel nuovo array gli oggetti conteneti le due variabili determinate 
+    array.push({nome, falliSubiti});  
+})
+
+console.log(soccer_team);
+console.log(array);
